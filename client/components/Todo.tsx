bc7edb9269515815b3c88162967e65da28e54a18
Paul David Tan <cryxuz@gmail.com>
2023-09-09 15:07:28 +1200

    add client side component Todo.tsx- webpage rendering

---
+++ client/components/Todo.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { useQuery, useMutation } from '@tanstack/react-query'
+import { getTasks } from '../apis/apiClient'
+
+export function Todo() {
+  const { data: todo, isLoading, error } = useQuery(['todo'], getTasks)
+  if (error) {
+    return <p>Something went wrong.</p>
+  }
+  if (isLoading || !todo) {
+    return <p>Loading...Please wait.</p>
+  }
+  return (
+    <div>
+      {todo.map((el: any) => {
+        return <li key={todo.id}>{el.task}</li>
+      })}
+    </div>
+  )
+}

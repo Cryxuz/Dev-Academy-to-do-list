277963513ac1651b7b1a5e2b0c017997efb9e417
Paul David Tan <cryxuz@gmail.com>
2023-09-12 09:07:13 +1200

    line through on check checkbox

--- client/components/Todo.tsx
+++ client/components/Todo.tsx
@@ -5,12 +5,13 @@ import { Tasks } from '../../models/todos'
 import { EditTasks } from './EditTasks'
 
 export function Todo() {
+  const [checkedTasks, setCheckedTasks] = useState([])
+
   const { data: todo, isLoading, error } = useQuery(['todo'], getTasks)
   const queryClient = useQueryClient()
   const delMutation = useMutation(delTask, {
     onSuccess: () => {
       // Refetching the tasks after a successful mutation to update the data
-      console.log('hello')
       queryClient.invalidateQueries(['todo'])
     },
   })
@@ -19,20 +20,27 @@ export function Todo() {
     return <p>Something went wrong.</p>
   }
   if (isLoading || !todo) {
-    return <p>Loading...Please wait.</p>
+    return <p>Loading... Please wait.</p>
   }
 
   // Handling the delete
-
   async function handleDelete(
     event: React.MouseEvent<HTMLButtonElement>,
     taskId: number,
   ) {
-    event.preventDefault() // this prevents the default form submission behavior
-
-    // console.log('delete button component')
-    // console.log(taskId)
+    event.preventDefault()
     delMutation.mutate(taskId)
+  }
+
+  // Function to handle checkbox change
+  function handleCheckboxChange(event, taskId) {
+    if (event.target.checked) {
+      // If checked, add the task id to the checkedTasks array
+      setCheckedTasks([...checkedTasks, taskId])
+    } else {
+      // If unchecked, remove the task id from the checkedTasks array
+      setCheckedTasks(checkedTasks.filter((id) => id !== taskId))
+    }
   }
 
   return (
@@ -45,8 +53,15 @@ export function Todo() {
               <button onClick={(event) => handleDelete(event, el.id)}>
                 Delete Task
               </button>
-              <input name="checkbox" type="checkbox" />
-              {el.task}
+              <input
+                name="checkbox"
+                type="checkbox"
+                checked={checkedTasks.includes(el.id)}
+                onChange={(event) => handleCheckboxChange(event, el.id)}
+              />
+              <span className={checkedTasks.includes(el.id) ? 'completed' : ''}>
+                {el.task}
+              </span>
             </li>
           )
         })}
